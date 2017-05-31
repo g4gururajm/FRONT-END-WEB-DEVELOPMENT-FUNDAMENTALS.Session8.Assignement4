73759669b391c305e46089ddbb6607c2f176f6da
g4gururajm <gururaj.tech@gmail.com>
2017-05-31 23:28:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+/* with id acadgild convert href from www.acadgild.com to jQuery tutorial  */
+$("acadgild").attr("href", "www.acadgild.com/")
+
+/* with id acadgild*/
+$("acadgild").text("Tutorial");
